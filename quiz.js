41cbe5df77c9c81f6591da04b7deae6f3b328e4b
Linux4Life531 <61314680+Linux4Life531@users.noreply.github.com>
2020-03-19 16:04:08 +0000

    Added the main files.

---
+++ quiz.js
@@ -0,0 +1,154 @@
+//
+
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+
+let questions = [
+    {
+        question : "What date was Alan Turing born?",
+        imgSrc : "img/Q1.png",
+        choiceA : "24 March 1913",
+        choiceB : "23 June 1912",
+        choiceC : "21 April 1911",
+        correct: "B"
+    }, {
+        question : "What college did he attend?",
+        imgSrc : "img/Q2.png",
+        choiceA : "Kings College",
+        choiceB : "Queens College",
+        choiceC : "Southern College",
+        correct: "A"
+    }, {
+        question : "What did he study at university?",
+        imgSrc : "img/Q3.png",
+        choiceA : "Computer & Data Science",
+        choiceB : "Physics",
+        choiceC : "Maths",
+        correct: "C"
+    }, {
+        question : "What code did he crack?",
+        imgSrc : "img/Q4.png",
+        choiceA : "Enigma",
+        choiceB : "Tyzoon",
+        choiceC : "Eginma",
+        correct: "A"
+    }, {
+        question : "Where did he crack his code?",
+        imgSrc : "img/Q5.png",
+        choiceA : "Benchly Park",
+        choiceB : "Bletchly Park",
+        choiceC : "Betty Park",
+        correct: "B"
+
+    }
+];
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+const gaugeWidth = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
+
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click",startQuiz);
+
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+}
+
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+function answerIsCorrect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+function answerIsWrong(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+}
